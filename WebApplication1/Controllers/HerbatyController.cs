d2e4490c22c1c0259dcc716d078e6780dd0feca9
Mateusz Flaga <m_flaga@wwsi365.edu.pl>
2020-09-08 20:01:06 +0200

    Dodaj pliki projektów.

---
+++ WebApplication1/Controllers/HerbatyController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HerbatyController : Controller
+    {
+        private HerbataDBContext db = new HerbataDBContext();
+
+        // GET: Herbaty
+        public ActionResult Index()
+        {
+            return View(db.Herbaty.ToList());
+        }
+
+        // GET: Herbaty/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            HerbatyModel herbatyModel = db.Herbaty.Find(id);
+            if (herbatyModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(herbatyModel);
+        }
+
+        // GET: Herbaty/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Herbaty/Create
+        // Aby zapewnić ochronę przed atakami polegającymi na przesyłaniu dodatkowych danych, włącz określone właściwości, z którymi chcesz utworzyć powiązania.
+        // Aby uzyskać więcej szczegółów, zobacz https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Nazwa,Cena,Zdjęcie")] HerbatyModel herbatyModel)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Herbaty.Add(herbatyModel);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(herbatyModel);
+        }
+
+        // GET: Herbaty/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            HerbatyModel herbatyModel = db.Herbaty.Find(id);
+            if (herbatyModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(herbatyModel);
+        }
+
+        // POST: Herbaty/Edit/5
+        // Aby zapewnić ochronę przed atakami polegającymi na przesyłaniu dodatkowych danych, włącz określone właściwości, z którymi chcesz utworzyć powiązania.
+        // Aby uzyskać więcej szczegółów, zobacz https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Nazwa,Cena,Zdjęcie")] HerbatyModel herbatyModel)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(herbatyModel).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(herbatyModel);
+        }
+
+        // GET: Herbaty/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            HerbatyModel herbatyModel = db.Herbaty.Find(id);
+            if (herbatyModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(herbatyModel);
+        }
+
+        // POST: Herbaty/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            HerbatyModel herbatyModel = db.Herbaty.Find(id);
+            db.Herbaty.Remove(herbatyModel);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ WebApplication1/Models/HerbatyModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace WebApplication1.Models
+{
+    public class HerbatyModel
+    {
+        // model do bazy danych
+        public int ID { get; set; }
+        public string Nazwa { get; set; }
+        public string Cena { get; set; }
+        public string Zdjęcie { get; set; }
+
+    }
+
+    public class HerbataDBContext : DbContext
+    {
+        public DbSet<HerbatyModel> Herbaty { get; set; }
+    }
+}
